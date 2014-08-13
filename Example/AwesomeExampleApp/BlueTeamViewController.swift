9afb9d122ddb64fd10be7f485ed2286d9e2b51f5
Matthew Delves <matt@reformedsoftware.com>
2014-08-13 20:12:57 +1000

    Add in example app

---
+++ Example/AwesomeExampleApp/BlueTeamViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BlueTeamViewController.swift
+//  AwesomeExampleApp
+//
+//  Created by Matthew Delves on 13/08/2014.
+//  Copyright (c) 2014 Matthew Delves. All rights reserved.
+//
+
+import UIKit
+
+class BlueTeamViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = UIColor.blueColor()
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  /*
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+  // Get the new view controller using segue.destinationViewController.
+  // Pass the selected object to the new view controller.
+  }
+  */
+  
+}
